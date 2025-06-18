25965cd0c97ac57e3fe4635eeab29ec4b21755b6
jp <jp@hoepfner.org>
2025-06-18 13:43:29 +0200

    added system.primaryUser line 43

--- darwin.nix
+++ darwin.nix
@@ -40,6 +40,7 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
+  system.primaryUser = user;
 
   # nix.configureBuildUsers = true; # outdated
 
