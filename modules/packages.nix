98f542bf387e7958696e127c93d66ac703eeb56e
jan.hoepfner <jp@hoepfner.org>
2025-10-13 21:39:44 +0200

    fix: arc-browser

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,49 +1,14 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    arc-browser
-    bartender
-    bat
-    boundary
-    bruno
     cnquery
     cnspec
-    direnv
-    discord
-    docker-client
-    drawio
-    eza
-    fd
-    fzf
     fzf-zsh
-    git
-    go
-    inetutils
-    iterm2
-    jq
-    k9s
-    kubectl
-    kubernetes-helm
-    mani
-    mas
     nerd-fonts.meslo-lg
     nix-direnv
     nix-index
+    nixd
     nixfmt-rfc-style
-    nmap
-    obsidian
     oh-my-zsh
-    pre-commit
-    pyenv
-    python313
-    raycast
-    spotify
-    terraform
-    tilt
-    tree
-    unixtools.watch
-    wget
-    yq-go
-    zoom-us
     zsh-autosuggestions
     zsh-fzf-tab
     zsh-powerlevel10k
